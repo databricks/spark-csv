db2ef19ff36e8f3c39dcde7ce5e437eea2cbbd18
Hossein <hossein@databricks.com>
2015-03-26 00:21:51 -0700

    Added Java unit tests for save

--- src/test/java/com/databricks/spark/csv/JavaCsvSuite.java
+++ src/test/java/com/databricks/spark/csv/JavaCsvSuite.java
@@ -2,22 +2,22 @@ package com.databricks.spark.csv;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.test.TestSQLContext$;
-import static org.apache.spark.sql.functions.*;
-
 
 public class JavaCsvSuite {
   private transient SQLContext sqlContext;
 
   String carsFile = "src/test/resources/cars.csv";
+
+  private String tempDir = "target/test/csvData/";
 
   @Before
   public void setUp() {
@@ -46,5 +46,17 @@ public class JavaCsvSuite {
     DataFrame df = sqlContext.load("com.databricks.spark.csv", options);
     int result = df.select("year").collect().length;
     Assert.assertEquals(result, 2);
+  }
+
+  @Test
+  public void testSave() {
+    DataFrame df = (new CsvParser()).withUseHeader(true).csvFile(sqlContext, carsFile);
+    TestUtils.deleteRecursively(new File(tempDir));
+    df.select("year", "model").save(tempDir, "com.databricks.spark.csv");
+
+    DataFrame newDf = (new CsvParser()).csvFile(sqlContext, tempDir);
+    int result = newDf.select("C1").collect().length;
+    Assert.assertEquals(result, 2);
+
   }
 }
