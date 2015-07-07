332c78e42e990be9e4f4d1f3dcae13d4097ff4b1
David Peacock <david.peaco@gmail.com>
2015-07-07 23:37:41 +0100

    Scalastyle issues. Update license + scalastyle config to allow more parameters.

--- src/main/scala/com/databricks/spark/csv/util/InferSchema.scala
+++ src/main/scala/com/databricks/spark/csv/util/InferSchema.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Databricks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.databricks.spark.csv.util
 
 import org.apache.spark.rdd.RDD
@@ -22,13 +37,14 @@ private[csv] object InferSchema {
     val rootTypes: Array[DataType] = tokenRdd.map { tokens =>
       tokens.map(inferField)
     }.treeReduce {
-      case (firstTypeArray, secondTypeArray) => firstTypeArray.zipAll(secondTypeArray, NullType, NullType).map { case ((a, b)) =>
-        val tpe = HiveTypeCoercion.findTightestCommonType(a, b).getOrElse(StringType)
-        tpe match {
-          case _: NullType => StringType
-          case other => other
+      case (firstTypeArray, secondTypeArray) =>
+        firstTypeArray.zipAll(secondTypeArray, NullType, NullType).map { case ((a, b)) =>
+          val tpe = HiveTypeCoercion.findTightestCommonType(a, b).getOrElse(StringType)
+          tpe match {
+            case _: NullType => StringType
+            case other => other
+          }
         }
-      }
     }
 
     val stuctFields = header.zip(rootTypes).map { case (thisHeader, rootType) =>
